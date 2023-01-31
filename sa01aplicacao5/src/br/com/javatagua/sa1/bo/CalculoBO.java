b4428c11758e918015952f5c49c4a2baec685000
AminGabriel <121125088+AminGabriel@users.noreply.github.com>
2023-01-31 01:03:29 -0300

    Delete sa01aplicacao5 directory

--- sa01aplicacao5/src/br/com/javatagua/sa1/bo/CalculoBO.java
+++
@@ -1,8 +0,0 @@
-package br.com.javatagua.sa1.bo;
-
-public class CalculoBO {
-
-	public double calcular(double valor1, double valor2, double valor3) {
-		return (valor1 + valor2 + valor3) / 3;
-	}
-}
